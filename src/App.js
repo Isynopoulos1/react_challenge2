b35bfb29c070ca650299792e33c1141879f78126
Isela <isela.v.alarcon@gmail.com>
2021-10-14 16:40:37 +0200

    First commit

---
+++ src/App.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+// IMPORT STYLES
+import "./styles/app.css";
+
+// IMPORT COMPONENTS
+import Logo from "./components/Logo";
+
+const App = () => {
+  return (
+    <main className="main">
+      <Logo />
+    </main>
+  );
+};
+
+export default App;
---
+++ src/components/Logo.js
@@ -0,0 +1,12 @@
+import React from "react";
+
+const Logo = () => {
+  return (
+    <img
+      src="https://ik.imagekit.io/ppayaz/ppayaz-website/paypal-logo_2_38uvSe1MY.png?updatedAt=1634221510370"
+      alt="logo"
+    />
+  );
+};
+
+export default Logo;
